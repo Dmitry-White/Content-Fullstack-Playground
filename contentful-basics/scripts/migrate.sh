110d7bd0badf7b55adfe96d2e72cc4cf98b7a333
Dmitry White <dmitry.b.white@gmail.com>
2023-06-24 22:40:26 +0100

    Add Contentful Basics category and landing page migrations

--- contentful-basics/scripts/migrate.sh
+++ contentful-basics/scripts/migrate.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 SPACE_ID=$1
-FILE_PATH=$2
+ENV_ID=$2
+FILE_PATH=$3
 
 echo "Script running..."
 echo "Migration: $FILE_PATH..."
 
 contentful space migration \
     --space-id=$SPACE_ID \
+    --environment-id=$ENV_ID \
     --yes \
     $FILE_PATH
 
