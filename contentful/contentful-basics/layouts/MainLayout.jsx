1507a12020573a65538dc9eafe41f534ccdf22da
Dmitry White <dmitry.b.white@gmail.com>
2023-09-23 19:27:16 +0100

    Update Contentful Basics main layout

--- contentful/contentful-basics/layouts/MainLayout.jsx
+++ contentful/contentful-basics/layouts/MainLayout.jsx
@@ -1,6 +1,22 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
+const Header = () => (
+  <div className="p-2 bg-blau text-white">
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+);
+
+const Footer = () => (
+  <div className="mt-4 p-2 bg-blau text-white">
+    <a href="https://training.contentful.com" target="_blank" rel="noreferrer">
+      Contentful Learning Services
+    </a>
+  </div>
+);
+
 const MainLayout = (props) => {
   return (
     <>
@@ -8,25 +24,13 @@ const MainLayout = (props) => {
         <title>Jumpstart Shop</title>
       </Head>
 
-      <div className="mt-4 p-2 bg-blau text-white">
-        <Link href="/">
-          <a>Nav</a>
-        </Link>
-      </div>
+      <Header />
 
       <main>
         <div className="h-screenx py-20 px-60"> {props.children}</div>
       </main>
 
-      <div className="mt-4 p-2 bg-blau text-white">
-        <a
-          href="https://training.contentful.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Contentful Learning Services
-        </a>
-      </div>
+      <Footer />
     </>
   );
 };
